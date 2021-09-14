9d373c9b90b0366f12e215263a195e50c52dbf34
JYe16 <54547626+JYe16@users.noreply.github.com>
2021-09-14 10:03:19 -0700

    Sound file updated

--- Assets/Scripts/GoalsController.cs
+++ Assets/Scripts/GoalsController.cs
@@ -9,9 +9,7 @@ public class GoalsController : MonoBehaviour
     public int value = 10;          // earning score after capture this goal
     public float moveSpeed = 18.0f;      // move speed of the goal
     public float minDistance = 100.0f;    // lower than minDistance, stop dragging back
-
-    // TODO: sound effect
-    // public AudioClip capturedAudio; 
+    AudioSource goal_audio;
 
     // Update is called once per frame
     void Update()
@@ -20,12 +18,17 @@ public class GoalsController : MonoBehaviour
         minDistance = 100.0f;
         GameObject spaceship = GameObject.Find("Spaceship");
         float distance = Vector3.Distance(transform.position, spaceship.transform.position);   // calculate the distance between goal and spaceship
+
         if (checkReached(distance, minDistance))
         {
             transform.LookAt(spaceship.transform);
             transform.eulerAngles = new Vector3(0.0f, transform.eulerAngles.y, 0.0f);
             transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }else{
+            goal_audio = GetComponent<AudioSource>();
+            goal_audio.Play();
+            
+            //TODO: Add a delay here
             Destroy(gameObject);
             //update score
             UpdateScore.score += value;
