d3b8f8b5919d3389dbbed005d1da3cad8d536818
JYe16 <54547626+JYe16@users.noreply.github.com>
2021-10-16 17:29:55 -0700

    Update Utils.cs

--- Assets/Scripts/Public/Utils.cs
+++ Assets/Scripts/Public/Utils.cs
@@ -14,7 +14,6 @@ public static class Utils
     public static string ReadDataFromFile(string fileName)
     {
         string path = GetFilePath(fileName);
-        Debug.Log(path);
         if (File.Exists(path))
         {
             using (StreamReader reader = new StreamReader(path))
