c5ad0e768e103552e0a992888682d8e7903f0796
Barry Yip <ys5851@163.com>
2021-10-23 18:24:35 -0700

    add explosion fracture effects

--- Assets/Scripts/Goals/GoalMove.cs
+++ Assets/Scripts/Goals/GoalMove.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 //using TreeEditor;
 using UnityEngine;
 
@@ -9,7 +10,10 @@ public class GoalMove : MonoBehaviour
     [HideInInspector]public GameObject target;
     [HideInInspector]public AudioClip destroyGoalAudio;
     [HideInInspector]public GameObject explosion;
-    
+    public GameObject fracturedPieces;
+    public GameObject originalGoal;
+    public float force;
+    public float radius;
     public float moveSpeed;
     
     private float distance;
@@ -17,6 +21,7 @@ public class GoalMove : MonoBehaviour
     private bool isHide = false;
     private RotateBySelf rotateEffect;
     private float minDistance = 180.0f;
+    private float destroyPiecesDuration = 5.0f;
     
     void Start()
     {
@@ -42,24 +47,55 @@ public class GoalMove : MonoBehaviour
             if(!isHide) goalValue.CapturedEffect();
             // hide laser line
             target.GetComponent<LaserLine>().enabled = false;
+            DestroyGoal();
         }
+    }
+
+    private void OnMouseDown()
+    {
+        ExplosionAndHide();
+    }
+
+    private void DestroyGoal()
+    {
+        foreach (Transform child in transform) {
+            Destroy(child.gameObject);
+        }
+        Destroy(gameObject);
     }
 
     public void ExplosionAndHide()
     {
-        StartCoroutine(GoalExplosion());
+        isHide = true;
+        originalGoal.GetComponent<Renderer>().enabled = false;
+        Explode();
     }
 
-    IEnumerator GoalExplosion()
+    private void Explode()
     {
+        fracturedPieces.SetActive(true);
         Vector3 screenCenter = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-        isHide = true;
+        // play explosion fire effect
         GameObject bombObj = Instantiate(explosion, screenCenter, Quaternion.identity);
-        Destroy(bombObj, 3f);
+        Destroy(bombObj, destroyPiecesDuration / 2);
+        // play explosion sound effect
         if(destroyGoalAudio != null && PlayerPrefs.GetInt("sound") == 1) 
             AudioSource.PlayClipAtPoint(destroyGoalAudio, Camera.main.transform.position, 0.3f);
-        gameObject.GetComponent<Renderer>().enabled = false;
-        yield return new WaitForSeconds(1);
+        // add force to pieces
+        Vector3 boundCenter = originalGoal.GetComponent<MeshFilter>().sharedMesh.bounds.center;
+        Vector3 center = fracturedPieces.transform.localToWorldMatrix.MultiplyPoint(boundCenter);
+        Transform rootTrans = fracturedPieces.transform;
+        for (int i = 0; i < rootTrans.childCount; i++)
+        {
+            Transform pieceTrans = rootTrans.GetChild(i);
+            Rigidbody body = pieceTrans.GetComponent<Rigidbody>();
+            if (body != null)
+            {
+                body.AddExplosionForce(force, center, radius);
+                Destroy(rootTrans.GetChild(i), destroyPiecesDuration);
+            }
+        }
+        // change speed to lase line speed after explosion
         moveSpeed = Gloable.LASER_LINE_MOVE_SPEED;
     }
 }
