a23e1416cf049e29a305c86213cbe1850d137334
Jackie Ye <jackie.ye.97@outlook.com>
2021-10-26 15:33:26 -0700

    audio fade out functionality added

--- Assets/Scripts/Level/GameManager.cs
+++ Assets/Scripts/Level/GameManager.cs
@@ -111,7 +111,10 @@ public class GameManager : MonoBehaviour
                 break;
             case GameState.GameOver:
                 GameObject playMusic = GameObject.Find("gamePlayMusic");
-                Destroy(playMusic.gameObject);
+                //use dofade
+                AudioSource playMusicSource = playMusic.GetComponent<AudioSource>();
+                playMusicSource.DOFade(0, 2);
+                //Destroy(playMusic.gameObject);
                 Tutorial.isPlaying = false;
                 SceneManager.LoadScene("GameOver");
                 break;
